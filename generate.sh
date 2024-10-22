d061be12e00c88f7be25e6b3e41196dd2e25454b
alex <test@MacBook-Air-test.local>
2024-10-22 21:09:08 +0300

    rename .sh file+ add dir

--- generate.sh
+++ generate.sh
@@ -4,6 +4,6 @@ protoc  --go_out=./  \
 
 protoc --doc_out=. --doc_opt=markdown,README.md ./society.proto
 
-protoc  --go_out=./  \
-        --go-grpc_out=./ \
+#protoc  --go_out=./  \
+#        --go-grpc_out=./ \
         #new_user.proto
