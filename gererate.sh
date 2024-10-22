75da02396eff3daae0ce844adb53710e34204c2e
alex <test@MacBook-Air-test.local>
2024-10-22 19:53:55 +0300

    create society.proto

---
+++ gererate.sh
@@ -0,0 +1,9 @@
+protoc  --go_out=./  \
+        --go-grpc_out=./ \
+        society.proto
+
+protoc --doc_out=. --doc_opt=markdown,README.md ./society.proto
+
+protoc  --go_out=./  \
+        --go-grpc_out=./ \
+        #new_user.proto
---
+++ go.mod
@@ -0,0 +1,5 @@
+module society-proto
+
+go 1.23.1
+
+require google.golang.org/protobuf v1.35.1 // indirect
